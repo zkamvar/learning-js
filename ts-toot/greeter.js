377bd5302e0ec97d1aeac85dfbf1d7569360da70
Zhian N. Kamvar <zkamvar@gmail.com>
2019-07-22 09:59:45 +0100

    add class

--- ts-toot/greeter.js
+++ ts-toot/greeter.js
@@ -1,5 +1,18 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
-    return "Hello, " + person;
+    return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = "Jane User";
+// Just a dict with firstName and lastName will work because the interface is
+// the same.
+// let user = { firstName: "Jane", lastName: "User" };
+// document.body.textContent = greeter(user);
+var user = new Student("Jane", "M.", "Student");
 document.body.textContent = greeter(user);
--- ts-toot/greeter.ts
+++ ts-toot/greeter.ts
@@ -1,8 +1,18 @@
+class Student {
+  fullName: string;
+  constructor(
+    public firstName:     string,
+    public middleInitial: string,
+    public lastName:      string,
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
 // An interface allows us to use typescript with the *shape* of an object, but
 // the class doesn't particularly matter
 interface Person {
   firstName: string;
-  lastName: string;
+  lastName:  string;
 }
 
 function greeter(person: Person) {
@@ -11,6 +21,9 @@ function greeter(person: Person) {
 
 // Just a dict with firstName and lastName will work because the interface is
 // the same.
-let user = { firstName: "Jane", lastName: "User" };
+// let user = { firstName: "Jane", lastName: "User" };
+// document.body.textContent = greeter(user);
+
+let user = new Student("Jane", "M.", "Student");
 
 document.body.textContent = greeter(user);
