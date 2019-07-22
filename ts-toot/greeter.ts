71d19a3165710a4d53882617fe9f82dd385cbeb0
Zhian N. Kamvar <zkamvar@gmail.com>
2019-07-22 09:53:21 +0100

    add interface example

--- ts-toot/greeter.ts
+++ ts-toot/greeter.ts
@@ -1,7 +1,16 @@
-function greeter(person) {
-  return "Hello, " + person;
+// An interface allows us to use typescript with the *shape* of an object, but
+// the class doesn't particularly matter
+interface Person {
+  firstName: string;
+  lastName: string;
 }
 
-let user = "Jane User";
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+// Just a dict with firstName and lastName will work because the interface is
+// the same.
+let user = { firstName: "Jane", lastName: "User" };
 
 document.body.textContent = greeter(user);
